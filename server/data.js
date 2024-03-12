12983a508eb060b481663812727a5291f7f84d51
yas3jb <yasmu213@gmail.com>
2024-03-11 22:20:44 -0400

    added more products

--- server/data.js
+++ server/data.js
@@ -37,10 +37,61 @@ const dummyData = async () => {
           "https://m.media-amazon.com/images/I/51YFNAqxKDL._AC_SY780_.jpg",
       }),
       createProduct({
-        name: "smartphone",
-        description: "Latest smartphone model with advanced features.",
-        price: 699.0,
-        imageUrl: "https://m.media-amazon.com/images/I/51UtM-A3fdL.jpg",
+        name: "Smart LED Light",
+        description: "50ft Smart Led Strip Lights for Bedroom",
+        price: 13.99,
+        imageUrl:
+          "https://m.media-amazon.com/images/I/81mLzAeSdoL._AC_UF894,1000_QL80_.jpg",
+      }),
+      createProduct({
+        name: "Power Bank",
+        description: "Portable Charger 26800mAh 22.5W Fast Charging Battery",
+        price: 32.99,
+        imageUrl:
+          "https://m.media-amazon.com/images/I/81D9rG8YbgL._AC_UF894,1000_QL80_.jpg",
+      }),
+      createProduct({
+        name: "Beats Studio Buds",
+        description:
+          "True Wireless Noise Cancelling Earbuds, Enhanced Apple & Android",
+        price: 169.95,
+        imageUrl:
+          "https://m.media-amazon.com/images/I/51AN8ExPAVL._AC_UF894,1000_QL80_.jpg",
+      }),
+      createProduct({
+        name: "Portable Bluetooth Speaker",
+        description: "Portable Wireless Bluetooth 5.0 Speaker",
+        price: 44.99,
+        imageUrl:
+          "https://m.media-amazon.com/images/I/81-AAu018UL._AC_UF894,1000_QL80_.jpg",
+      }),
+      createProduct({
+        name: "GoPro HERO12 Black",
+        description: "Waterproof Action Camera with 5.3K60 Ultra HD Video",
+        price: 349.0,
+        imageUrl:
+          "https://m.media-amazon.com/images/I/71p5V8+OnfL._AC_UF894,1000_QL80_.jpg",
+      }),
+      createProduct({
+        name: "Security Camera",
+        description: "Security Camera Outdoor Wired with Night Vision Color",
+        price: 31.99,
+        imageUrl:
+          "https://m.media-amazon.com/images/I/61c3t9sd5tL._AC_UF894,1000_QL80_.jpg",
+      }),
+      createProduct({
+        name: "Neck Massager",
+        description: "Nekteck Shiatsu Deep Kneading Massage Pillow with Heat",
+        price: 49.99,
+        imageUrl:
+          "https://m.media-amazon.com/images/I/71TmeJpNJCL._AC_UF1000,1000_QL80_.jpg",
+      }),
+      createProduct({
+        name: "Car Charger",
+        description: "Car Charger, 67W 3-Port Compact Fast Charger",
+        price: 39.99,
+        imageUrl:
+          "https://m.media-amazon.com/images/I/61fcdbI0u7L._AC_UF894,1000_QL80_.jpg",
       }),
     ]);
 
